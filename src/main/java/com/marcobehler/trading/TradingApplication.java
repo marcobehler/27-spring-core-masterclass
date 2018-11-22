40716385aeb1b4d82d2e348d7967f5fb0ad25888
Marco Behler <marco@marcobehler.com>
2018-11-22 10:39:05 +0100

    [initial project skeleton]

---
+++ src/main/java/com/marcobehler/trading/TradingApplication.java
@@ -0,0 +1,11 @@
+package com.marcobehler.trading;
+
+/**
+ * Thanks for watching this episode! If you want to, drop me a line to info@marcobehler.com.
+ */
+public class TradingApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
