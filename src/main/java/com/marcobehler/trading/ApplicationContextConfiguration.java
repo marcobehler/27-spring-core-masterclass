0309b5b8c1288b14f7d18eb3a923b740cfc1574b
Marco Behler <marco@marcobehler.com>
2018-12-08 12:04:53 +0100

    [Episode 1.5] Understanding Spring Transaction Management

--- src/main/java/com/marcobehler/trading/ApplicationContextConfiguration.java
+++ src/main/java/com/marcobehler/trading/ApplicationContextConfiguration.java
@@ -1,19 +1,20 @@
 package com.marcobehler.trading;
 
 
+import org.h2.Driver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
-import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.SQLFeatureNotSupportedException;
-import java.util.logging.Logger;
 
 @Configuration
 @ComponentScan
+@EnableTransactionManagement
 public class ApplicationContextConfiguration {
 
     @Bean
@@ -23,54 +24,16 @@ public class ApplicationContextConfiguration {
     }
 
 
+    @Bean
+    public PlatformTransactionManager txManager() {
+        return new DataSourceTransactionManager(dataSource());
+    }
 
-
-    private DataSource dataSource() {
-        return new DataSource() {
-            @Override
-            public Connection getConnection() throws SQLException {
-                return null;
-            }
-
-            @Override
-            public Connection getConnection(String username, String password) throws SQLException {
-                return null;
-            }
-
-            @Override
-            public <T> T unwrap(Class<T> iface) throws SQLException {
-                return null;
-            }
-
-            @Override
-            public boolean isWrapperFor(Class<?> iface) throws SQLException {
-                return false;
-            }
-
-            @Override
-            public PrintWriter getLogWriter() throws SQLException {
-                return null;
-            }
-
-            @Override
-            public void setLogWriter(PrintWriter out) throws SQLException {
-
-            }
-
-            @Override
-            public void setLoginTimeout(int seconds) throws SQLException {
-
-            }
-
-            @Override
-            public int getLoginTimeout() throws SQLException {
-                return 0;
-            }
-
-            @Override
-            public Logger getParentLogger() throws SQLFeatureNotSupportedException {
-                return null;
-            }
-        };
+    @Bean
+    public DataSource dataSource() {
+        SimpleDriverDataSource ds = new SimpleDriverDataSource();
+        ds.setDriver(new Driver());
+        ds.setUrl("jdbc:h2:mem:");
+        return ds;
     }
 }
--- src/main/java/com/marcobehler/trading/UserDao.java
+++ src/main/java/com/marcobehler/trading/UserDao.java
@@ -1,7 +1,12 @@
 package com.marcobehler.trading;
 
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
 import javax.sql.DataSource;
 
+@Component
 public class UserDao {
 
     private final DataSource dataSource;
@@ -10,7 +15,9 @@ public class UserDao {
         this.dataSource = dataSource;
     }
 
+    @Transactional
     public void save(User user) {
+        System.out.println("Transaction open? " + TransactionSynchronizationManager.isActualTransactionActive());
         System.out.println("saving user to the database via datasource " + dataSource + " : user.getEmail() = " + user.getEmail());
         // TODO saving to the db here....
     }
