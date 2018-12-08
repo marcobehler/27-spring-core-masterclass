c995238b189dff5115d7188ca03c39af6056e09f
Marco Behler <marco@marcobehler.com>
2018-12-08 14:46:22 +0100

    [Episode 1.5] Understanding Spring Transaction Management

--- src/main/java/com/marcobehler/trading/TradingApplication.java
+++ src/main/java/com/marcobehler/trading/TradingApplication.java
@@ -1,6 +1,7 @@
 package com.marcobehler.trading;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 /**
  * Thanks for watching this episode! If you want to, drop me a line to info@marcobehler.com.
@@ -12,8 +13,13 @@ public class TradingApplication {
         System.out.println("user.getEmail() = " + user.getEmail());
 
 
+        System.out.println("Transaction open? " + TransactionSynchronizationManager.isActualTransactionActive());
+
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(ApplicationContextConfiguration.class);
         UserService userService = ctx.getBean(UserService.class);
         userService.register(user);
+
+        System.out.println("Transaction open? " + TransactionSynchronizationManager.isActualTransactionActive());
+
     }
 }
--- src/main/java/com/marcobehler/trading/UserService.java
+++ src/main/java/com/marcobehler/trading/UserService.java
@@ -2,10 +2,13 @@ package com.marcobehler.trading;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 @Component
 //@Service
-public class UserService implements IUserService {
+@Transactional
+public class UserService {
 
     @Autowired
     private MailService mailService;
@@ -13,16 +16,21 @@ public class UserService implements IUserService {
     @Autowired // @Inject..@Resource
     private UserDao userDao;
 
-    @Override
+
     public void register(User user) {
+
+        System.out.println("Transaction open? " + TransactionSynchronizationManager.isActualTransactionActive());
+
         mailService.sendWelcomeEmail(user);
         userDao.save(user);
+
+        System.out.println("Transaction open? " + TransactionSynchronizationManager.isActualTransactionActive());
         // 1. save the user to a database.persistent storage..
         // 2. send the user a welcome email
         // 3..risk /balance check....
     }
 
-    @Override
+
     public void closeAccount(User user) {
         System.out.println("Closing the users account: " + user.getEmail());
     }
